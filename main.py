752b408d9e95213c1d8c0dea5a28b3bda9b62525
Samuel Eisenhandler <sgeisenhandler@gmail.com>
2023-01-09 18:29:08 -0500

    Progress on high-performance representation

--- main.py
+++ main.py
@@ -6,9 +6,8 @@
 - print to terminal
 """
 
+import math
 import random
-from functools import partial
-
 
 LEFT = 0
 DOWN = 1
@@ -85,6 +84,8 @@ def left(board):
 
 
 def rotate(board, rotations):
+    # four 32-bit ints
+
     for _ in range(rotations):
         board = list(map(list, list(zip(*board[::-1]))))
     return board
@@ -203,6 +204,42 @@ class OnePly:
         return f"OnePly({self.heuristic.name()})"
 
 
+class MultiPly4:
+    def __init__(self, heuristic):
+        self.heuristic = heuristic
+
+    def choose_move(self, board):
+        def inner_choose_move(board, depth):
+            if depth == 0:
+                return self.heuristic.score(board)
+            best_score = None
+            for direction in DIRECTIONS:
+                if not move_is_valid(board, direction):
+                    continue
+                new_board = move(board, direction)
+                if depth >= 1:
+                    score = inner_choose_move(new_board, depth - 1)
+                score = self.heuristic.score(new_board)
+                if best_score is None or score > best_score:
+                    best_score = score
+            return best_score
+
+        best_dir = None
+        best_score = None
+        for direction in DIRECTIONS:
+            if not move_is_valid(board, direction):
+                continue
+            new_board = move(board, direction)
+            score = inner_choose_move(new_board, 4)
+            if best_score is None or score > best_score:
+                best_dir = direction
+                best_score = score
+        return best_dir
+
+    def name(self):
+        return f"MultiPly4({self.heuristic.name()})"
+
+
 def highest_tile(board):
     return max(col for row in board for col in row if col is not None)
 
@@ -237,8 +274,90 @@ def compare_strategies(strategies):
         )
 
 
+def numeric_representation(board):
+    row_bits = []
+    for row in board:
+        row_num = 0
+        for i, elem in enumerate(row):
+            row_num += (elem.bit_length() - 1) << (3 - i) * 8 if elem is not None else 0
+        row_bits.append(row_num)
+    return tuple(row_bits)
+
+
+def create_rotate_rep():
+    code = "def rotate_rep(rep): return ("
+    rows = []
+    for row_idx in range(4):
+        row_components = []
+        for col_idx in range(4):
+            value = f"(((rep[{3 - col_idx}] >> {8 * (3 - row_idx)}) & 0xff) << {8 * (3 - col_idx)})"
+            row_components.append(value)
+        rows.append(" + ".join(row_components))
+    code += ", ".join(rows)
+    code += ")"
+
+    return code
+
+
+def left_rep(rep):
+    result = []
+    for row in rep:
+        c1 = (row >> 24) & 0xFF
+        c2 = (row >> 16) & 0xFF
+        c3 = (row >> 8) & 0xFF
+        c4 = row & 0xFF
+        row = [value for value in (c1, c2, c3, c4) if value != 0]
+        ret = 0
+        shift = 24
+        i = 0
+        while i < len(row) - 1:
+            a = row[i]
+            b = row[i + 1]
+            print(i, a, b)
+            if a == b:
+                value = a + 1
+                i += 1
+            else:
+                value = a
+            ret += value << shift
+            shift -= 8
+            i += 1
+        result.append(ret)
+    return tuple(result)
+
+
+def print_numeric_representation(row_bits):
+    for n in row_bits:
+        s = ""
+        for i in range(4):
+            s += str((n >> (3 - i) * 8) & 0xFF) + ", "
+        print(s)
+
+
+print(create_rotate_rep())
+exec(create_rotate_rep())
+rep = numeric_representation(starting_board)
+print_numeric_representation(rep)
+lefted = left_rep(rep)
+print_numeric_representation(lefted)
+
+quit()
+
+
+row_bits = numeric_representation(starting_board)
+rotate_numeric_representation(row_bits)
+print_numeric_representation(row_bits)
+
+quit()
+
 compare_strategies(
-    [OnePly(HighestTile()), OnePly(EmptySpace()), OnePly(Combined()), Random()]
+    [
+        OnePly(HighestTile()),
+        OnePly(EmptySpace()),
+        OnePly(Combined()),
+        MultiPly4(Combined()),
+        Random(),
+    ]
 )
 
 
